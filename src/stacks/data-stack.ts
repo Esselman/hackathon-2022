52cdddf504ee12605decb45aa6a3157c60a62ef6
Chris Brown <chris.brown@ncino.com>
2022-08-29 10:01:54 -0400

    Add historical table

--- src/stacks/data-stack.ts
+++ src/stacks/data-stack.ts
@@ -10,5 +10,27 @@ export class DataStack extends Stack {
     super(scope, id, props);
     this.feature = scope;
     this.removalPolicy = Utility.isProductionEnvironment() ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY;
+
+    this.buildHistoryTable();
+  }
+
+  private buildHistoryTable(): DynamoDb {
+    const tableName: string = this.getFullName('StatusLink');
+    const historyTable: DynamoDb = new DynamoDb(this, tableName, {
+      tableName: tableName,
+      partitionKey: {
+        name: 'ownerId',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'status',
+        type: AttributeType.STRING
+      },
+      removalPolicy: this.removalPolicy
+    });
+
+    this.feature.addServiceAccess(historyTable.tableArn, ['dynamodb:*']);
+
+    return historyTable;
   }
 }

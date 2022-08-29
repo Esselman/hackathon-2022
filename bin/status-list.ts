2c191d15dea9a0c0d0fc44edc5c04d26f0c63019
Chris Brown <chris.brown@ncino.com>
2022-08-29 10:01:35 -0400

    Add data stack

--- bin/status-list.ts
+++ bin/status-list.ts
@@ -5,6 +5,7 @@ import { RemovalPolicy } from '@aws-cdk/core';
 import { ComputeType } from '@aws-cdk/aws-codebuild';
 import { LambdaStack } from '../src/stacks/lambda-stack';
 import { ApiStack } from '../src/stacks/api-stack';
+import { DataStack } from '../src/stacks/data-stack';
 
 const deployAccount = process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT;
 const deployRegion = process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION;
@@ -21,6 +22,16 @@ let removalPolicy: RemovalPolicy = RemovalPolicy.RETAIN;
 if (feature.getContext('temporary', false)) {
   removalPolicy = RemovalPolicy.DESTROY;
 }
+
+console.log('🛠  Data Stack');
+const dataStack = new DataStack(feature, `${feature.getFullName('DataStack')}`, {
+  description: 'Required. Contains data storage resources for status-list.',
+  env: {
+    account: deployAccount,
+    region: deployRegion
+  }
+});
+feature.setStack('dataStack', dataStack);
 
 console.log('🛠 Lambda Stack');
 const lambdaStack = new LambdaStack(feature, `${feature.getFullName('LambdaStack')}-${stageName}`, {
